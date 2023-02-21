34759c5fb3c0cbb674cc6c331b7a42f8663be072
Rosa Codina <rosa.codina90@gmail.com>
2023-02-21 16:16:36 +0100

    Improve design counters

--- components/eit-counter.js
+++ components/eit-counter.js
@@ -6,17 +6,35 @@ export class EitCounter extends LitElement {
       :host {
         display: inline-block;
         padding: 1em;
-        margin-left: 5px;
+        margin-left: 8px;
         border: 1px solid #ccc;
         text-align: center;
-        width: 200px;
+        width: 250px;
       }
+
       h2 {
         color: red;
       }
+
       .paragraph {
         color: blue;
         font-size: 1.5em;
+      }
+
+      p {
+        color: blue;
+      }
+
+      input {
+        width: 55px;
+        font-size: 1.5em;
+        padding: 0.5em;
+        text-align: center;
+      }
+
+      button {
+        height: 25px;
+        width: 40px;
       }
 
       @media (min-width: 500px) {
@@ -30,6 +48,7 @@ export class EitCounter extends LitElement {
   static get properties() {
     return {
       counter: { type: Number }, //property component
+      reflect: true,
     };
   }
 
@@ -40,19 +59,30 @@ export class EitCounter extends LitElement {
 
   render() {
     return html`
-      <h2>My counter</h2>
+      <h2>Counter</h2>
+      <p>
+        Type a number in the input and then press the button below it to
+        increase o decrease the counter.
+      </p>
       <p class="paragraph">${this.counter}</p>
-      <button @click=${this.increase}>+1</button>
-      <button @click=${this.decrease}>+1</button>
+      <p>
+        <input id="quantity" type="number" value="1" />
+      </p>
+      <button @click=${this.increase}>+ 1</button>
+      <button @click=${this.decrease}>- 1</button>
     `;
   }
 
+  get quantity() {
+    return this.shadowRoot.getElementById('quantity').value;
+  }
+
   increase() {
-    this.counter++;
+    this.counter += parseInt(this.quantity);
   }
 
   decrease() {
-    this.counter--;
+    this.counter -= parseInt(this.quantity);
   }
 }
 customElements.define('eit-counter', EitCounter);
